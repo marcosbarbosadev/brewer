f46b253023d0b658a252f1292bf772a1d0e8a2cb
Marcos Barbosa <barbosa.mg@gmail.com>
2019-02-01 00:17:29 -0200

    Configurando a ViewResolver com Thymeleaf

--- src/main/java/com/algaworks/brewer/config/init/AppInitializer.java
+++ src/main/java/com/algaworks/brewer/config/init/AppInitializer.java
@@ -13,7 +13,7 @@ public class AppInitializer extends AbstractAnnotationConfigDispatcherServletIni
 
 	@Override
 	protected Class<?>[] getServletConfigClasses() {
-		return new Class<?>[]{ WebConfig.class };
+		return new Class<?>[] { WebConfig.class };
 	}
 
 	@Override
