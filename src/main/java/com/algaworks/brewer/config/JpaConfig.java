86ae56fa5dd88389f696e1fe4e18c29031e0b729
Marcos Barbosa <barbosa.mg@gmail.com>
2019-07-28 15:15:56 -0300

    Adicionando dependência do Spring data JPA e realizando as configurações

---
+++ src/main/java/com/algaworks/brewer/config/JpaConfig.java
@@ -0,0 +1,45 @@
+package com.algaworks.brewer.config;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import com.algaworks.brewer.model.Cerveja;
+
+@Configuration
+public class JpaConfig {
+
+	@Bean
+	public DataSource dataSource() {
+		JndiDataSourceLookup dataSourceLookup = new JndiDataSourceLookup();
+		dataSourceLookup.setResourceRef(true);
+		return dataSourceLookup.getDataSource("jdbc/brewerDB");
+	}
+
+	@Bean
+	public JpaVendorAdapter jpaVendorAdapter() {
+		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+		adapter.setDatabase(Database.MYSQL);
+		adapter.setShowSql(true);
+		adapter.setGenerateDdl(false);
+		adapter.setDatabasePlatform("org.hibernate.dialect.MySQLDialect");
+		return adapter;
+	}
+	
+	@Bean
+	public EntityManagerFactory entityManagerFactory(DataSource dataSource, JpaVendorAdapter jpaVenderAdapter) {
+		LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+		factory.setDataSource(dataSource);
+		factory.setJpaVendorAdapter(jpaVenderAdapter);
+		factory.setPackagesToScan(Cerveja.class.getPackage().getName());
+		factory.afterPropertiesSet();
+		return factory.getObject();
+	}
+}
